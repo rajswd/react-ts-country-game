7b5a1df86609c1ca6802eac36582875fe009bfc9
rajswd <raj.swd1@gmail.com>
2022-05-21 16:40:08 +0800

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,72 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  const data = {
+    Canada: 'Ottawa',
+    China: 'Beijing',
+    India: 'New Delhi',
+    Malaysia: 'Kualalumpur',
+    Germany: 'Berlin',
+    'New Zealand': 'Wellington',
+  };
+  const [gameData, setGameData] = React.useState([]);
+  const [actionData, setActionData] = React.useState('');
+  const [errData, setErrData] = React.useState([]);
+
+  React.useEffect(() => {
+    const d = [];
+    for (let i in data) {
+      d.push(i, data[i]);
+    }
+    d.sort();
+    setGameData(d);
+  }, []);
+
+  const clearActionData = () => {
+    setActionData(null);
+    setErrData([]);
+  };
+
+  const onBtnClick = (e) => {
+    const tBtn = e.target.id;
+    if (!actionData) {
+      setActionData(tBtn);
+      setErrData([]);
+    } else if (data[tBtn] === actionData || data[actionData] === tBtn) {
+      const d = gameData.filter((item) => item !== tBtn && item !== actionData);
+      setGameData(d);
+      clearActionData();
+    } else {
+      setErrData([actionData, tBtn]);
+      setActionData('');
+    }
+  };
+
+  return (
+    <div>
+      {gameData.length > 0 ? (
+        gameData.map((item) => {
+          return (
+            <button
+              onClick={onBtnClick}
+              className={
+                actionData === item
+                  ? 'selected-cl'
+                  : errData.includes(item)
+                  ? 'error-cl'
+                  : ''
+              }
+              id={item}
+            >
+              {' '}
+              {item}
+            </button>
+          );
+        })
+      ) : (
+        <p>Congratulation</p>
+      )}
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-4b6yau
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-4b6yau)
